aa2a085d48cf41b3b75915fdccbfe56bb3b5bc91
bdgarcia <brunodg91@gmail.com>
2019-05-04 19:46:02 -0300

    progreso en templates

--- application/models.py
+++ application/models.py
@@ -3,3 +3,9 @@ from django.db import models
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField("date created", auto_now_add=True)
+
+class ItemsCatalogo(models.Model):
+    nombre = "nombre de residencia"
+    ubicacion = "ubicacion"
+    precio = "precio"
+    descripcion = "descripcion"
--- application/views.py
+++ application/views.py
@@ -3,12 +3,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Greeting
+from .models import ItemsCatalogo
 
 
 # Create your views here.
 def index(request):
-    # return HttpResponse('Hello from Python!')
-    return render(request, "index.html")
+
+    residencias = ItemsCatalogo()
+
+    return render(request, "index.html", {"residencias": residencias})
 
 # Create your views here.
 def test(request):
