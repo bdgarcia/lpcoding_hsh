b98ba2669aed5b2c756c589942d0c9feff9d8b61
bdgarcia <brunodg91@gmail.com>
2019-05-16 22:51:02 -0300

    cambie los forms del /admin para que funcionen con el nuevo modelo

--- modelos/admin.py
+++ modelos/admin.py
@@ -1,16 +1,30 @@
 from django.contrib import admin
+from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
-
-from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .forms import UserCreationForm, UserChangeForm
 from .models import Usuario
 
 
 class CustomUserAdmin(UserAdmin):
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
-    model = Usuario
-    list_display = ['email', 'username']
+    # The forms to add and change user instances
+    form = UserChangeForm
+    add_form = UserCreationForm
+    list_display = ("username", "type",)
+    ordering = ("email",)
+
+    fieldsets = (
+        (None, {'fields': ('username', 'type', 'email', 'fecha_nacimiento', 'creditos', 'nombre', 'apellido', 'is_superuser', 'is_staff', 'is_active')}),
+        )
+    add_fieldsets = ((None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2', 'type', 'email', 'fecha_nacimiento', 'creditos', 'is_superuser', 'is_staff', 'is_active')}
+            ),
+        )
+
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ()
 
 
 admin.site.register(Usuario, CustomUserAdmin)
