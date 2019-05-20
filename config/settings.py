d67b746bd3d6a7e42e4d52b0848df9527dc350b9
Sebastian <39461749+sebasvillena@users.noreply.github.com>
2019-05-20 11:34:19 -0300

    Agregado iconito

--- config/settings.py
+++ config/settings.py
@@ -119,6 +119,9 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
+""" STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+] """
 
 #Ruta a la carpeta donde se almacenaran las imagenes
 MEDIA_ROOT = os.path.join(BASE_DIR, 'application', 'static', 'media')
